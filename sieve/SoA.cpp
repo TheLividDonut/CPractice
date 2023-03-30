2f808aa010e7e76d099f38297b50f87706f9baa4
TheLividDonut <88906140+TheLividDonut@users.noreply.github.com>
2023-03-29 17:40:13 -0700

    Sieve is done

--- sieve/SoA.cpp
+++ sieve/SoA.cpp
@@ -6,22 +6,13 @@ void initArray() {
     for(int i = 0; i < 200; i++) {
         if(i == 1 || i == 2) {
             soa[i] = false;
-            std::cout << i + " = false ";
         }
        else {
             soa[i] = true;
-            std::cout << i + " = true";
         } 
     }
 }
-/* void checkValue(int ion) {
-    for(int i = ion + 1; i < 200; i++) {
-        if(i % ion == 0) {
-            soa[i] = false;
-            std::cout << soa[i];
-        }
-    }
-} */
+
 void checkValue() {
     for(long outer = 2; outer < 200; outer++) {
         for(long i = outer + 1; i < 200; i++) {
@@ -31,16 +22,22 @@ void checkValue() {
         }
     }
 }
+
+void returnBool(int index) {
+    if(soa[index] == 1) {
+        std::cout << "True";
+    }
+    else {
+        std::cout << "False";
+    }
+}
+
 int main() {
-    std::cout << "Starting check";
     initArray();
-    //for(int i = 0; i < 200; i++) {
-        //checkValue(3);
-         //std::cout << soa[9];
-    //}
     checkValue();
-    std::cout << '\n';
     for(int i = 0; i < 200; i++) {
-        std::cout << i + " ";
+        std::cout << i + 1 << " ";
+        returnBool(i);
+        std::cout << '\n';
     }
 } 
